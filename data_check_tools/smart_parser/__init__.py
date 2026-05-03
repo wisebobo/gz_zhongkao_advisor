d0a21fda05febd4b88f0b26ea21351feb0c26df6
BennyLBS <wisebobo@163.com>
2026-05-03 21:06:35 +0800

    chore: 更新__init__.py导出ElementClassifier

--- data_check_tools/smart_parser/__init__.py
+++ data_check_tools/smart_parser/__init__.py
@@ -3,11 +3,11 @@ PDF OCR 智能文档解析系统
 基于 PaddleOCR PP-Structure 的文档版面分析和表格识别
 """
 
-# 逐步导入已完成的模块
+# 已完成的模块
 from .layout_analyzer import DocumentLayoutAnalyzer
+from .element_classifier import ElementClassifier
 
 # TODO: 后续添加其他模块
-# from .element_classifier import ElementClassifier
 # from .table_extractor import TableStructureExtractor
 # from .article_extractor import ArticleTextExtractor
 # from .output_manager import SeparatedOutputManager
@@ -16,7 +16,7 @@ from .layout_analyzer import DocumentLayoutAnalyzer
 __version__ = "1.0.0"
 __all__ = [
     "DocumentLayoutAnalyzer",
-    # "ElementClassifier", 
+    "ElementClassifier",
     # "TableStructureExtractor",
     # "ArticleTextExtractor",
     # "SeparatedOutputManager",
